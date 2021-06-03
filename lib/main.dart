2de5a03f23f72dcf403a1d9b447f0ce4fa0eb020
unknown <almayra123@gmail.com>
2021-06-03 16:02:33 +0700

    [INPUT]: add addon input

--- lib/main.dart
+++ lib/main.dart
@@ -94,6 +94,272 @@ class MyApp extends StatelessWidget {
         borderRadius: BorderRadius.circular(8)),
   );
 
+  final InputDecoration prefixInput = InputDecoration(
+      hintText: "Example",
+      labelStyle: TextStyle(color: Color(0xFF40A9FF)),
+      prefixIcon: Icon(
+        Icons.folder_open_outlined,
+        color: Colors.grey,
+      ),
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey, width: 1.0),
+          borderRadius: BorderRadius.circular(8)),
+      focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF40A9FF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)));
+
+  final InputDecoration suffixInput = InputDecoration(
+      hintText: "Example",
+      labelStyle: TextStyle(color: Color(0xFF40A9FF)),
+      suffixIcon: Icon(
+        Icons.folder_open_outlined,
+        color: Colors.grey,
+      ),
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey, width: 1.0),
+          borderRadius: BorderRadius.circular(8)),
+      focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF40A9FF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)));
+
+  final InputDecoration prefixSuffixInput = InputDecoration(
+      hintText: "Example",
+      labelStyle: TextStyle(color: Color(0xFF40A9FF)),
+      prefixIcon: Icon(
+        Icons.folder_open_outlined,
+        color: Colors.grey,
+      ),
+      suffixIcon: Icon(
+        Icons.close,
+        color: Colors.grey,
+      ),
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey, width: 1.0),
+          borderRadius: BorderRadius.circular(8)),
+      focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF40A9FF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)));
+
+  final InputDecoration prefixAddonInput = InputDecoration(
+      hintText: "Example",
+      labelStyle: TextStyle(color: Colors.black),
+      isDense: true,
+      prefixIcon: Padding(
+        padding: EdgeInsets.only(right: 12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color(0xFFBFBFBF),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                      bottomLeft: Radius.circular(8)),
+                  color: gray020),
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text(
+                  "Https://",
+                  style: TextStyle(color: gray100),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFFBFBFBF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)),
+      focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF40A9FF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)));
+
+  final InputDecoration suffixAddonInput = InputDecoration(
+      hintText: "Example",
+      labelStyle: TextStyle(color: Colors.black),
+      isDense: true,
+      suffixIcon: Padding(
+        padding: EdgeInsets.only(left: 12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color(0xFFBFBFBF),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(8),
+                      bottomRight: Radius.circular(8)),
+                  color: gray020),
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Icon(
+                  Icons.close,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFFBFBFBF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)),
+      focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF40A9FF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)));
+
+  final InputDecoration prefixSuffixAddonInput = InputDecoration(
+      hintText: "Example",
+      labelStyle: TextStyle(color: Colors.black),
+      isDense: true,
+      prefixIcon: Padding(
+        padding: EdgeInsets.only(right: 12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color(0xFFBFBFBF),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                      bottomLeft: Radius.circular(8)),
+                  color: gray020),
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text(
+                  "Https://",
+                  style: TextStyle(color: gray100),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      suffixIcon: Padding(
+        padding: EdgeInsets.only(left: 12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color(0xFFBFBFBF),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(8),
+                      bottomRight: Radius.circular(8)),
+                  color: gray020),
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Icon(
+                  Icons.close,
+                  color: Colors.grey,
+                  size: 16.0,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFFBFBFBF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)),
+      focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF40A9FF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)));
+
+  final InputDecoration prefixAddonRightInput = InputDecoration(
+      hintText: "Example",
+      labelStyle: TextStyle(color: Colors.black),
+      isDense: true,
+      prefixIcon: Icon(
+        Icons.folder_open_outlined,
+        color: Colors.grey,
+      ),
+      suffixIcon: Padding(
+        padding: EdgeInsets.only(left: 12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color(0xFFBFBFBF),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(8),
+                      bottomRight: Radius.circular(8)),
+                  color: gray020),
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Icon(
+                  Icons.close,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFFBFBFBF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)),
+      focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF40A9FF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)));
+
+  final InputDecoration suffixAddonLeftInput = InputDecoration(
+      hintText: "Example",
+      labelStyle: TextStyle(color: Colors.black),
+      isDense: true,
+      suffixIcon: Icon(
+        Icons.folder_open_outlined,
+        color: Colors.grey,
+      ),
+      prefixIcon: Padding(
+        padding: EdgeInsets.only(right: 12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color(0xFFBFBFBF),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                      bottomLeft: Radius.circular(8)),
+                  color: gray020),
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text(
+                  "Https://",
+                  style: TextStyle(color: gray100),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFFBFBFBF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)),
+      focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFF40A9FF), width: 1.0),
+          borderRadius: BorderRadius.circular(8)));
+
   @override
   Widget build(BuildContext context) => Storybook(
         children: [
@@ -293,107 +559,58 @@ class MyApp extends StatelessWidget {
                                 ),
                               ),
                               Option(
-                                "Prefix Icon",
+                                "Affix - Prefix",
                                 TextField(
-                                  decoration: InputDecoration(
-                                      hintText: "Example",
-                                      labelStyle:
-                                          TextStyle(color: Color(0xFF40A9FF)),
-                                      prefixIcon: Icon(
-                                        Icons.folder_open_outlined,
-                                        color: Colors.grey,
-                                      ),
-                                      enabledBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                              color: Colors.grey, width: 1.0),
-                                          borderRadius:
-                                              BorderRadius.circular(8)),
-                                      focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                              color: Color(0xFF40A9FF),
-                                              width: 1.0),
-                                          borderRadius:
-                                              BorderRadius.circular(8))),
+                                  decoration: this.prefixInput,
                                   keyboardType: TextInputType.name,
                                 ),
                               ),
                               Option(
-                                "Suffix Icon",
+                                "Affix - Suffix",
                                 TextField(
-                                  decoration: InputDecoration(
-                                      hintText: "Example",
-                                      labelStyle:
-                                          TextStyle(color: Color(0xFF40A9FF)),
-                                      suffixIcon: Icon(
-                                        Icons.folder_open_outlined,
-                                        color: Colors.grey,
-                                      ),
-                                      enabledBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                              color: Colors.grey, width: 1.0),
-                                          borderRadius:
-                                              BorderRadius.circular(8)),
-                                      focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                              color: Color(0xFF40A9FF),
-                                              width: 1.0),
-                                          borderRadius:
-                                              BorderRadius.circular(8))),
+                                  decoration: this.suffixInput,
                                   keyboardType: TextInputType.name,
                                 ),
                               ),
                               Option(
-                                "Prefix Text",
+                                "Affix - Both",
                                 TextField(
-                                  decoration: InputDecoration(
-                                      hintText: "Example",
-                                      labelStyle:
-                                          TextStyle(color: Colors.black),
-                                      isDense: true,
-                                      prefixIcon: Padding(
-                                        padding: EdgeInsets.only(right: 12.0),
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Container(
-                                              decoration: BoxDecoration(
-                                                  border: Border.all(
-                                                    color: Color(0xFFBFBFBF),
-                                                    width: 1.0,
-                                                  ),
-                                                  borderRadius:
-                                                      BorderRadius.only(
-                                                          topLeft: Radius
-                                                              .circular(8),
-                                                          bottomLeft:
-                                                              Radius.circular(
-                                                                  8)),
-                                                  color: gray020),
-                                              child: Padding(
-                                                padding: EdgeInsets.all(15.0),
-                                                child: Text(
-                                                  "Https://",
-                                                  style:
-                                                      TextStyle(color: gray100),
-                                                ),
-                                              ),
-                                            )
-                                          ],
-                                        ),
-                                      ),
-                                      enabledBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                              color: Color(0xFFBFBFBF),
-                                              width: 1.0),
-                                          borderRadius:
-                                              BorderRadius.circular(8)),
-                                      focusedBorder: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                              color: Color(0xFF40A9FF),
-                                              width: 1.0),
-                                          borderRadius:
-                                              BorderRadius.circular(8))),
+                                  decoration: this.prefixSuffixInput,
+                                  keyboardType: TextInputType.name,
+                                ),
+                              ),
+                              Option(
+                                "Addon - Prefix",
+                                TextField(
+                                  decoration: this.prefixAddonInput,
+                                  keyboardType: TextInputType.name,
+                                ),
+                              ),
+                              Option(
+                                "Addon - Suffix",
+                                TextField(
+                                  decoration: this.suffixAddonInput,
+                                  keyboardType: TextInputType.name,
+                                ),
+                              ),
+                              Option(
+                                "Addon - Both",
+                                TextField(
+                                  decoration: this.prefixSuffixAddonInput,
+                                  keyboardType: TextInputType.name,
+                                ),
+                              ),
+                              Option(
+                                "Prefix - Addon - Right",
+                                TextField(
+                                  decoration: this.prefixAddonRightInput,
+                                  keyboardType: TextInputType.name,
+                                ),
+                              ),
+                              Option(
+                                "Suffix - Addon - Left",
+                                TextField(
+                                  decoration: this.suffixAddonLeftInput,
                                   keyboardType: TextInputType.name,
                                 ),
                               ),
