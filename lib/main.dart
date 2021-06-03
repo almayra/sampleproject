9d0d3b87841ebb812596a5b17ca955899134c81a
unknown <almayra123@gmail.com>
2021-06-03 17:20:18 +0700

    [SEARCH INPUT]: added search input

--- lib/main.dart
+++ lib/main.dart
@@ -360,6 +360,30 @@ class MyApp extends StatelessWidget {
           borderSide: BorderSide(color: Color(0xFF40A9FF), width: 1.0),
           borderRadius: BorderRadius.circular(8)));
 
+  final InputDecoration searchInput = InputDecoration(
+    hintText: "Search",
+    labelStyle: TextStyle(color: Color(0xFF40A9FF)),
+    enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+        borderRadius: BorderRadius.circular(8)),
+    focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color(0xFF40A9FF), width: 1.0),
+        borderRadius: BorderRadius.circular(8)),
+    disabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color(0xFFD9D9D9), width: 1.0),
+        borderRadius: BorderRadius.circular(8)),
+    prefixIcon: Icon(
+      Icons.search,
+      color: Colors.grey,
+    ),
+    suffix: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+        child: Icon(
+          Icons.close,
+          size: 16,
+        )),
+  );
+
   @override
   Widget build(BuildContext context) => Storybook(children: [
         Story(
@@ -620,6 +644,9 @@ class MyApp extends StatelessWidget {
             maxLength: 500,
             keyboardType: TextInputType.name,
           ),
-        )
+        ),
+        Story.simple(
+            name: "Search Input",
+            child: TextFormField(decoration: this.searchInput))
       ]);
 }
