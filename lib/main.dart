f91a624d7c037d232dbe4b98dc792f90a41b9b8a
unknown <almayra123@gmail.com>
2021-06-09 20:23:12 +0700

    add avatar

--- lib/main.dart
+++ lib/main.dart
@@ -1987,6 +1987,63 @@ class _MyAppState extends State<MyApp> {
                               )),
                         ])
                   ],
+                )),
+        Story(
+            name: "Avatar",
+            builder: (_, k) => Column(
+                  children: [
+                    k.options(
+                        label: "type",
+                        initial: Stack(
+                          children: [
+                            CircleAvatar(
+                                backgroundImage: AssetImage('assets/Image.png'),
+                                radius: 50),
+                            Positioned(
+                                bottom: 0,
+                                right: 4,
+                                child: Icon(Icons.add_circle,
+                                    color: primary400, size: 24.0))
+                          ],
+                        ),
+                        options: [
+                          Option(
+                            "Type one",
+                            Stack(
+                              children: [
+                                CircleAvatar(
+                                    backgroundImage:
+                                        AssetImage('assets/Image.png'),
+                                    radius: 50),
+                                Positioned(
+                                    bottom: 0,
+                                    right: 4,
+                                    child: Icon(Icons.add_circle,
+                                        color: primary400, size: 24.0))
+                              ],
+                            ),
+                          ),
+                          Option(
+                            "Type two",
+                            Stack(
+                              children: [
+                                CircleAvatar(
+                                    radius: 50,
+                                    backgroundColor: Color(0xFF007FF4),
+                                    child: CircleAvatar(
+                                        backgroundImage:
+                                            AssetImage('assets/Image.png'),
+                                        radius: 49)),
+                                Positioned(
+                                    bottom: 0,
+                                    right: 4,
+                                    child: Icon(Icons.add_a_photo_rounded,
+                                        color: primary400, size: 24.0))
+                              ],
+                            ),
+                          )
+                        ])
+                  ],
                 ))
       ]);
 }
